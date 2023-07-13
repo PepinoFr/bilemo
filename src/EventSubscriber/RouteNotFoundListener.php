99986238ea1c9b2a2c0f3bf3da7c5be29f561cbb
IC-Mathieu <mathieu@inovea-conseil.com>
2023-07-13 23:32:13 +0200

    finish

---
+++ src/EventSubscriber/RouteNotFoundListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class RouteNotFoundListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'kernel.exception' => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $response = new JsonResponse(['error' => 'Route not found'], 404);
+            $event->setResponse($response);
+        }
+    }
+}
